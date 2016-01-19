67d4a6055b71cac43e3c4b71867210760acf6e2a
Nick Stefan <NickStefan12@gmail.com>
2016-01-19 09:01:45 -0800

    expand find to handle more complex selectors
    These changes will allow these to work:
    ```
    find( 'div.test-class[data-test-attribute]' )
    ```
    There is a breaking change in that the selector is then on the elements object as THE EXACT selector (do not slice off the leading ".").
    ```
    this.elements[ 'div.test-class[data-test-attribute]' ];
    ```

--- src/middleware/find.js
+++ src/middleware/find.js
@@ -1,20 +1,55 @@
-import TestUtils from 'react-addons-test-utils'
+import TestUtils from 'react-addons-test-utils';
+import _ from 'lodash';
 
 export default function find(selector){
 
-  let elements, name
-  if (!(typeof selector === "string")) {
-    name = (selector.name || selector.displayName).toLowerCase()
-    elements = TestUtils.scryRenderedComponentsWithType(this.instance, selector)
-  } else if (selector.match(/\./)) {
-    selector = selector.replace(/\./, '')
-    elements = TestUtils.scryRenderedDOMComponentsWithClass(this.instance, selector)
-  }
-  else elements = TestUtils.scryRenderedDOMComponentsWithTag(this.instance, selector)
+  var self = this;
+  var foundElements = [];
+  var tokens = selector.split(/(?=\.)|(?=#)|(?=\[)/);
+
+  tokens
+  .forEach(function(subselector, key, collection){
+      var els;
+      switch (subselector[0]){
+        // class
+        case '.':
+          els = TestUtils.scryRenderedDOMComponentsWithClass(self.instance, subselector.slice(1));
+            foundElements.push( Array.isArray(els) ? els : [els] );
+            break;
+
+        // id
+        case '#':
+          els = TestUtils.findAllInRenderedTree(self.instance, function(component){
+            if (component.id === subselector.slice(1)){
+              return true;
+            }
+          });
+          foundElements.push( Array.isArray(els) ? els : [els] );
+          break;
+
+        // data attribute
+        case '[':
+          els = TestUtils.findAllInRenderedTree(self.instance, function(component){
+            if (component.getAttribute) {
+              return component.getAttribute(subselector.slice(1,-1));
+            }
+          });
+          foundElements.push( Array.isArray(els) ? els : [els] );
+          break;
+
+        // tag
+        default:
+          els = TestUtils.scryRenderedDOMComponentsWithTag(self.instance, subselector);
+          foundElements.push( Array.isArray(els) ? els : [els] );
+          break;
+        }
+    });
+
+  var elements = _.intersection.apply(_, foundElements);
 
   if (Array.isArray(elements) && elements.length === 1) {
-    this.elements[name || selector] = elements[0]
+    this.elements[selector] = elements[0];
   } else {
-    this.elements[name || selector] = elements
+    this.elements[selector] = elements;
   }
 }
